b1b6032fc3d3e572b8d963f3633a9fd498f56ffd
Melvin Carvalho <melvincarvalho@gmail.com>
2016-02-29 02:51:45 +0100

    Add tests

--- test/util.js
+++ test/util.js
@@ -8,9 +8,19 @@ var expect = require('chai').expect;
 describe('Util Functions', function () {
 
   describe('hashToAddr', function() {
+    
     it('hashToAddr is a function', function () {
       expect( (util.hashToAddr)).to.be.a('function');
     });
+
+    it('hashToAddr hashes uncompressed correctly', function () {
+      expect(util.hashToAddr('test', true).toString()).to.equal('1HKqKTMpBTZZ8H5zcqYEWYBaaWELrDEXeE');
+    });
+
+    it('hashToAddr hashes compressed correctly', function () {
+      expect(util.hashToAddr('test').toString()).to.equal('19eA3hUfKRt7aZymavdQFXg5EZ6KCVKxr8');
+    });
+
   });
 
 });
